bce0a65ed47fbcd8f4ecadb2e9321582cf21b6fb
Leonardo <leonardof.rocio@gmail.com>
2020-11-11 10:57:47 -0300

    Sistema de Login incluso

--- js/autenticacao.js
+++ js/autenticacao.js
@@ -1,18 +1,96 @@
-var send = document.getElementById("send");
-var inputEmail = document.getElementById("inputEmail");
-var inputPassword = document.getElementById("inputPassword");
+function login() {
+  if (firebase.auth().currentUser) {
+    firebase.auth().signOut()
+  }
+  const email = document.getElementById("userEmail").value
+  const password = document.getElementById("userPass").value
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(() => {
+      swal.fire({
+        icon: "success",
+        title: "Login realizado com sucesso"
+      }).then(() => {
+        setTimeout(() => {
+          window.location.replace("../index.html")
+        }, 1000)
+      })
+    })
+    .catch(error => {
+      const errorCode = error.code
+      switch (errorCode) {
+        case "auth/wrong-password":
+          swal.fire({
+            icon: "error",
+            title: "Senha inválida",
+          })
+          break
+        case "auth/invalid-email":
+          swal.fire({
+            icon: "error",
+            title: "Email invalido",
+          })
+          break
+        case "auth/user-not-found":
+          swal.fire({
+            icon: "warning",
+            title: "Usuario não encontrado",
+            text: "Deseja criar um usuário?",
+            showCancelButton: true,
+            cancelButtonText: "Não",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Sim",
+            confirmButtonColor: "#3085d6",
+          }).then((result) => {
+            if (result.value) {
+              window.location.replace('../html/cadastro.html')
+            }
+          })
+          break
+        default:
+          swal.fire({
+            icon: "error",
+            title: error.message,
+          })
+      }
+    })
+}
 
-send.addEventListener('click', function () {
+function signUp() {
+  const senha = document.getElementById('userPass')
+  const email = document.getElementById('userEmail')
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email.value, senha.value)
+    .then(() => {
+      swal.fire({
+        icon: "success",
+        title: "Usuário criado com sucesso"
+      }).then(() => {
+        setTimeout(() => {
+          window.location.replace("./index.html")
+        }, 1000)
+      })
+    })
+    .catch((error) => {
+      const errorCode = error.code
+      switch (errorCode) {
+        case "auth/weak-password":
+          swal.fire({
+            icon: "error",
+            title: "Senha muito fraca",
+          })
+          break
+        default:
+          swal.fire({
+            icon: "error",
+            title: error.message,
+          })
+      }
+    })
+}
 
-    firebase.auth().signInWithEmailAndPassword(inputEmail.value, inputPassword.value).then( function (result) {
-        alert("Usuário logado com sucesso!");
-
-        window.location.replace("index.html");
-    }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-        alert(errorMessage);
-      });
-})
+function logout() {
+  firebase.auth().signOut()
+}
--- js/user.js
+++ js/user.js
@@ -1,18 +1,64 @@
-var send2 = document.getElementById("send2");
-var inputEmail2 = document.getElementById("inputEmail2");
-var inputPassword2 = document.getElementById("inputPassword2");
+let currentUser = {}
 
-send2.addEventListener('click', function () {
+function getUser() {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      currentUser.uid = user.uid
+      let userLabel = document.getElementById('botaoInscricao')
+      userLabel.innerHTML = user.email
+      createNewNavButton()
+      let logOutButton = document.getElementById('logOutBtn')
+      logOutButton.appendChild(createLogOutButton())
+    } else {
+      swal.fire({
+        icon: "warning",
+        title: "Usuario não está logado",
+        text: "Deseja logar em um usuário?",
+        showCancelButton: true,
+        cancelButtonText: "Não",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sim",
+        confirmButtonColor: "#3085d6",
+      }).then((result) => {
+        if (result.value) {
+          setTimeout(() => {
+            window.location.replace('./html/login.html')
+          })
+        }
+      })
+    }
+  })
+}
 
-    firebase.auth().createUserWithEmailAndPassword(inputEmail2.value, inputPassword2.value).then( function (result) {
-        alert("Usuário cadastro com sucesso!");
+function createLogOutButton() {
+  const newButton = document.createElement("button")
+  newButton.setAttribute('class', 'btn btn-outline-success my-2 my-sm-0 pl-4 pr-4 pt-3 pb-3')
+  newButton.setAttribute('id', 'botaoInscricao')
+  newButton.appendChild(document.createTextNode("Sair"))
+  newButton.setAttribute("onclick", "logout()")
+  return newButton
+}
 
-        window.location.replace("login.html");
-    }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-        alert(errorMessage);
-      });
-})
+function createNewNavButton() {
+  const itemList = document.getElementById('auth')
+  const newItem = document.createElement("li")
+  newItem.setAttribute('class', "list-inline-item")
+  newItem.setAttribute('id', 'logOutBtn')
+  itemList.appendChild(newItem)
+}
+
+function logout() {
+  swal.fire({
+    icon: 'success',
+    title: 'Usuário foi desconectado'
+  }).then((result) => {
+    if (result.value) {
+      firebase.auth().signOut()
+      window.location.reload("index.html")
+    }
+  })
+}
+
+window.onload = function () {
+  getUser()
+}
