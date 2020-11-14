aefcc3d47b7110129a61e478ed99a8a9c799bd3f
Leonardo <leonardof.rocio@gmail.com>
2020-11-14 11:59:50 -0300

    Funcionalidade da página de perfil

--- js/app.js
+++ js/app.js
@@ -3,10 +3,12 @@ var firebaseConfig = {
   authDomain: "jml-musculacao.firebaseapp.com",
   databaseURL: "https://jml-musculacao.firebaseio.com",
   projectId: "jml-musculacao",
-  storageBucket: "jml-musculacao.appspot.com",
+  storageBucket: "gs://jml-musculacao.appspot.com",
   messagingSenderId: "942758114362",
   appId: "1:942758114362:web:77638d14e485d678a3b103"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+
 
